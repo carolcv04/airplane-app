e98a4b00aaead1cda4ea53d1e30d3268dab9b2e2
Thomas Johnson <Thjohnson@chapman.edu>
2024-11-20 21:33:58 -0800

    Added CSVs to populate the tables

--- airplane_app.py
+++ airplane_app.py
@@ -16,9 +16,9 @@ def startScreen():
     db_ops.create_payment_option_table()
 
     #make sure to state the filepath inside the () ex. "/Users/carolina/DataGripProjects/CPSC408/RideshareLab/Data/rating.csv"
-    db_ops.populate_customer_table()
-    db_ops.populate_flight_table()
-    db_ops.populate_payment_option_table()
-    db_ops.populate_ticket_table()
-    db_ops.populate_seat_table()
-    db_ops.populate_plane_table()
+    db_ops.populate_customer_table("C:/Users/Thoma/OneDrive/Desktop/Airplane App/airplane-app/customer.csv")
+db_ops.populate_flight_table("C:/Users/Thoma/OneDrive/Desktop/Airplane App/airplane-app/flight.csv")
+db_ops.populate_payment_option_table("C:/Users/Thoma/OneDrive/Desktop/Airplane App/airplane-app/payment_option.csv")
+db_ops.populate_ticket_table("C:/Users/Thoma/OneDrive/Desktop/Airplane App/airplane-app/ticket.csv")
+db_ops.populate_seat_table("C:/Users/Thoma/OneDrive/Desktop/Airplane App/airplane-app/seat.csv")
+db_ops.populate_plane_table("C:/Users/Thoma/OneDrive/Desktop/Airplane App/airplane-app/plane.csv")
